6b8a1ccbe8b37d1cd38983d6b513a8cd2f1db5ec
寿宝江 <sbj@MacBook-Air.local>
2013-12-22 22:42:25 +0800

    fix PersonalCareerView

--- Connection/PersonalCareerView.h
+++ Connection/PersonalCareerView.h
@@ -10,7 +10,7 @@
 #import "Connection.h"
 #import "PersonalBasicInfo.h"
 
-@interface PersonalCareerView : UITableViewController
+@interface PersonalCareerView : UITableViewController <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *m_Object;
 @property (weak, nonatomic) IBOutlet UITextField *m_Company;
@@ -18,8 +18,12 @@
 @property (weak, nonatomic) IBOutlet UITextField *m_Position;
 @property (weak, nonatomic) IBOutlet UITextView *m_Experience;
 @property (weak, nonatomic) IBOutlet UITextView *m_Career;
+@property (weak, nonatomic) IBOutlet UITableViewCell *m_SkillCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *m_CareerCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *m_ExperienceCell;
 
 @property (nonatomic, assign) bool isEditMode;
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *m_StatusBtn;
 @property (nonatomic, retain) PersonalBasicInfo* basicInfo;
 @end
--- Connection/PersonalCareerView.m
+++ Connection/PersonalCareerView.m
@@ -35,22 +35,74 @@
     return UIBarPositionTop;
 }
 
+- (BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
 
 - (void) viewWillAppear:(BOOL)animated
 {
-    
+    self.navigationController.navigationBar.hidden = true;
+    [super viewWillAppear:animated];
+    [self updateCareer];
+    [self updateEditMode];
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    _isEditMode = false;
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 
+}
+
+- (void) updateEditMode
+{
+    BOOL enable;
+    if (_isEditMode) {
+        [_m_StatusBtn setTitle:@"完成"];
+        _m_SkillCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        _m_CareerCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        _m_ExperienceCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        enable = TRUE;
+    } else {
+        [_m_StatusBtn setTitle:@"编辑"];
+        _m_SkillCell.accessoryType = UITableViewCellAccessoryNone;
+        _m_CareerCell.accessoryType = UITableViewCellAccessoryNone;
+        _m_ExperienceCell.accessoryType =UITableViewCellAccessoryNone;
+        enable = FALSE;
+    }
+    _m_Object.enabled = enable;
+    _m_Company.enabled = enable;
+    _m_Position.enabled =enable;
+    
+    _m_Object.delegate = self;
+    _m_Company.delegate = _m_Position.delegate = self;
+}
+
+- (void) updateCareer
+{
+    if (_basicInfo.my_details == nil)
+        _basicInfo.my_details = [NSEntityDescription insertNewObjectForEntityForName:@"PersonalCareer" inManagedObjectContext:[DBHelper getContext]];
+    
+    [_m_Object setText:_basicInfo.my_details.goal];
+    [_m_Company setText:_basicInfo.my_details.company];
+    [_m_Position setText:_basicInfo.my_details.position];
+    
+    CCareer* career = [[CCareer alloc] init];
+    [career deserialize:_basicInfo.my_details.career];
+    _m_Career.text = [career toString];
+    CSkillSet* set = [[CSkillSet alloc] init];
+    [set deserialize:_basicInfo.my_details.skills];
+    _m_Skill.text = [set toString];
+    CExperience* exps = [[CExperience alloc] init];
+    [exps deserialize:_basicInfo.my_details.history];
+    _m_Experience.text = [exps toString];
 }
 
 
@@ -61,71 +113,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Table view data source
-/*
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
-}
-*/
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 - (void) setPersonalBasicInfo: (PersonalBasicInfo*) basicInfo
 {
     _basicInfo = basicInfo;
@@ -135,12 +122,23 @@
 {
     return _basicInfo;
 }
+- (IBAction)SwitchEditMode:(id)sender {
+    if (_isEditMode) {
+        _basicInfo.my_details.goal = _m_Object.text;
+        _basicInfo.my_details.company = _m_Company.text;
+        _basicInfo.my_details.position = _m_Position.text;
+        [DBHelper SaveAll];
+    }
+    _isEditMode = !_isEditMode;
+    [self updateEditMode];
+}
 
 - (void) SaveCareer: (NSArray*) items
 {
     CCareer* career = [[CCareer alloc] init];
     career.fields = [items mutableCopy];
     _basicInfo.my_details.career = [career serialize];
+    [DBHelper SaveAll];
 }
 
 - (void) SaveExperience: (NSArray*) items
@@ -148,6 +146,7 @@
     CExperience* exp = [[CExperience alloc] init];
     exp.items = [items mutableCopy];
     _basicInfo.my_details.history = [exp serialize];
+    [DBHelper SaveAll];
 }
 
 - (void) SaveSkill: (NSArray*) items
@@ -155,6 +154,7 @@
     CSkillSet* set = [[CSkillSet alloc] init];
     set.skills = [items mutableCopy];
     _basicInfo.my_details.skills = [set serialize];
+    [DBHelper SaveAll];
 }
 
 #pragma mark - Navigation
@@ -189,6 +189,13 @@
     }
 }
 
+- (BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender
+{
+    if (_isEditMode)
+        return TRUE;
+    else
+        return FALSE;
+}
 
 
 @end
